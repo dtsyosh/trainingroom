cfef5d7d9266e5f3cb3c8f92b9be668f50f06d08
Diego Tsuyoshi <diego.amorim@cg.agence.com.br>
2020-05-11 13:07:28 -0400

    Added - Styles to tabs and change structure

--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,17 @@ const router = express.Router();
 
 
 router.get('/', (req, res) => {
-  res.render('index', { title: "Diego Yosh" });
+  res.render('index', {
+    title: "Diego Yosh",
+    home: "is-active"
+  });
 })
 
 
 router.get('/clima', (req, res) => {
   res.render('./clima/index', {
     title: "Clima",
+    clima: "is-active"
   })
 })
 
