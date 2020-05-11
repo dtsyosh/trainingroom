62f4853347fe3a3fb640151821a7d58e3097563a
Diego Tsuyoshi <diego.amorim@cg.agence.com.br>
2020-05-11 11:12:02 -0400

    Fixed - Removing unused code

--- public/js/clima.js
+++ public/js/clima.js
@@ -1,10 +1,3 @@
-$body = $("body");
-
-$(document).on({
-  ajaxStart: function () { $body.addClass("loading"); },
-  ajaxStop: function () { $body.removeClass("loading"); }
-});
-
 $(document).ready(function () {
 
   // 'Enviar' clicked
