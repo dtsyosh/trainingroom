9f7c9f1e3295ffbc285996dee2ee83e2a376e821
Diego Tsuyoshi <diego.amorim@cg.agence.com.br>
2020-05-28 17:22:09 -0400

    Added - UTM Coordinates

--- src/utils/bioma.js
+++ src/utils/bioma.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const parseDMS = require('parse-dms');
+const proj4 = require('proj4');
 
 const initials = {
   Acre: 'AC',
@@ -31,10 +32,30 @@ const initials = {
   Tocantins: 'TO',
 };
 
+function isUTM(coordinates) {
+  return coordinates.split('S').length == 1 ? true : false;
+}
+
 module.exports = {
   async getData(coordinates) {
     try {
-      const { lat, lon } = parseDMS(coordinates);
+      let lat = null;
+      let lon = null;
+      if (isUTM(coordinates)) {
+        // parse utm
+
+        const coordinatesFormatted = coordinates
+          .split(' ')
+          .map((n) => parseInt(n));
+
+        const utm = '+proj=utm +zone=23 +south';
+        const wgs84 = '+proj=longlat +ellps=WGS84 +datum=WGS84  +no_defs';
+        [lon, lat] = proj4(utm, wgs84, coordinatesFormatted);
+      } else {
+        const result = parseDMS(coordinates);
+        lat = result.lat;
+        lon = result.lon;
+      }
 
       const { data } = await axios.get(
         `https://us1.locationiq.com/v1/reverse.php?key=eb12c387025af0&lat=${lat}&lon=${lon}&format=json`
